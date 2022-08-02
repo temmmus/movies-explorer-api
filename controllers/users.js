a5dbcca373f1fc1e9ed03a7b557d46014ea592f3
temmmus <temmmus@gmail.com>
2022-08-03 00:44:30 +0500

    test

--- controllers/users.js
+++ controllers/users.js
@@ -41,6 +41,8 @@ module.exports.createUser = (req, res, next) => {
 };
 
 module.exports.getUserInfo = (req, res, next) => {
+  console.log(req)
+  console.log(req.user._id)
   User.findById(req.user._id)
     .then((user) => {
       if (user === null) {
